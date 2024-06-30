a5d8f2a20d811cb4070b9d7b48eb6521b7da1290
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-06-29 23:34:01 -0600

    Create some util functions

---
+++ src/util.ts
@@ -0,0 +1,43 @@
+//
+
+/**
+ * Delays the process for an amount of time.
+ *
+ * @param ms The time in milliseconds to delay.
+ * @returns
+ */
+export const delay = (ms: number): Promise<void> => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+/**
+ * Returns a random number between min and max. The min is inclusive and the max is exclusive.
+ *
+ *
+ * @param min
+ * @param max
+ * @returns A random number between min and max.
+ */
+export const random = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+/**
+ * Shuffles an array.
+ *
+ * @param array
+ * @returns A shuffled array.
+ */
+export const shuffle = <T>(array: T[]): T[] => {
+  const result = [...array];
+
+  for (let i = 0; i < result.length; i++) {
+    const j = random(0, i);
+
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+
+  return result;
+};
