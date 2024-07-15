dd7cb7489781ff7cd907ed994f1526022ac7d7cb
Camilo Suárez Sandí <suarezsandicamilo@gmail.com>
2024-07-15 12:03:45 -0600

    Create credits page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,48 @@
 //
 
-import { Stack } from 'expo-router';
+import { Tabs } from 'expo-router';
+
+import Icon from '@expo/vector-icons/FontAwesome';
 
 const Layout = () => {
   return (
-    <Stack
+    <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarShowLabel: false,
       }}
     >
-      <Stack.Screen name='index' />
-      <Stack.Screen name='lesson' />
-      <Stack.Screen name='settings' />
-    </Stack>
+      <Tabs.Screen
+        name='index'
+        options={{
+          tabBarIcon: ({ color }) => {
+            return <Icon name='star' color={color} size={24} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name='lesson'
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name='settings'
+        options={{
+          tabBarIcon: ({ color }) => {
+            return <Icon name='gear' color={color} size={24} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name='credits'
+        options={{
+          tabBarIcon: ({ color }) => {
+            return <Icon name='file' color={color} size={24} />;
+          },
+        }}
+      />
+    </Tabs>
   );
 };
 
---
+++ app/credits.tsx
@@ -0,0 +1,5 @@
+//
+
+import { SettingsScreen } from '@/screens';
+
+export default SettingsScreen;
